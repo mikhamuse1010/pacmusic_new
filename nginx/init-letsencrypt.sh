693020de436554e65ec7b701e6fce5fdf48e13af
mikhamuse1010 <mikha.kristofer@gmail.com>
2025-09-25 23:17:55 +0700

    resolve ILE error

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -18,8 +18,8 @@ fi
 
 echo "### Downloading recommended TLS parameters ..."
 mkdir -p "$data_path/conf"
-curl -s [https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf] > "$data_path/conf/options-ssl-nginx.conf"
-curl -s [https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem] > "$data_path/conf/ssl-dhparams.pem"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
 echo
 
 echo "### Creating dummy certificate for $domains ..."
