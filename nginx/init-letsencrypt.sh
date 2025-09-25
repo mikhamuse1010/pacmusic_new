bd6bed76f80b9531d283d200a4a01653c3a590bc
mikhamuse1010 <mikha.kristofer@gmail.com>
2025-09-25 23:27:49 +0700

    update nginx and docker compose

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -8,9 +8,10 @@
 # domains=(pacmusic.15.134.86.46.nip.io stg.pacmusic.15.134.86.46.nip.io)
 #
 domains=(pacmusic.15.134.86.46.nip.io stg.pacmusic.15.134.86.46.nip.io)
-email="mikha.kristoferexample.com"
+email="mikha.kristofer@example.com"
 rsa_key_size=4096
 data_path="./certbot_data"
+staging=0 # Set to 1 if you're testing your setup to avoid hitting rate limits
 
 if [ -d "$data_path" ]; then
   echo ">>> Existing data found for $domains. Recreating certificates ..."
@@ -22,27 +23,33 @@ curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/c
 curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
 echo
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/${domains[0]}"
-mkdir -p "$data_path/conf/live/${domains[0]}"
-docker compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
+echo "### Creating dummy certificates for all domains ..."
+for domain in "${domains[@]}"; do
+  path="/etc/letsencrypt/live/$domain"
+  mkdir -p "$data_path/conf/live/$domain"
+  docker compose run --rm --entrypoint "\
+    openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+      -keyout '$path/privkey.pem' \
+      -out '$path/fullchain.pem' \
+      -subj '/CN=localhost'" certbot
+done
 echo
 
-echo "### Starting NGINX ..."
+echo "### Starting nginx ..."
 docker compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
-docker compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/${domains[0]}" certbot
+echo "### Deleting dummy certificates for all domains ..."
+for domain in "${domains[@]}"; do
+    docker compose run --rm --entrypoint "\
+      rm -Rf /etc/letsencrypt/live/$domain && \
+      rm -Rf /etc/letsencrypt/archive/$domain && \
+      rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+done
 echo
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-# Join domains to -d args
+echo "### Requesting Let's Encrypt certificates for all domains ..."
+#Join $domains to -d args
 domain_args=""
 for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
@@ -54,8 +61,12 @@ case "$email" in
   *) email_arg="--email $email" ;;
 esac
 
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
 docker compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
@@ -63,7 +74,7 @@ docker compose run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
-echo "### Reloading NGINX ..."
+echo "### Reloading nginx ..."
 docker compose exec nginx nginx -s reload
 
 echo "### Starting all services ..."
